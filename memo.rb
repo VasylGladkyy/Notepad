7213417759af2974ad9f3f4fbf718096653664d5
Vasyl Gladkyy <vasil.gladkiy@gmail.com>
2019-10-04 19:36:49 +0300

    Implement methods read_from_console and to_strings in class Memo

--- memo.rb
+++ memo.rb
@@ -1,9 +1,19 @@
 class Memo<Post
 
   def read_from_console
+    puts "Нова замітка (Рахуються всі слова до рядка \"end\"):"
+    @text=[]
+    line = nil
+    while line!="end"do
+      line=STDIN.gets.chomp
+      @text<<line
+    end
+    @text.pop
   end
 
-  def to_string
+  def to_strings
+    time_string="Створено: #{@created_at.strftime("#{self.class.name}_%Y-%m-%d_%H-%M-%S.txt")} \n\r \n\r"
+    return @text.unshift(time_string)
   end
 
 end
